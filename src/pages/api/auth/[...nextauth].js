bc69542f05f705e6f83829a1b28c30cc0d84c3f8
ohaki <oom20170@hotmail.com>
2022-07-28 05:05:07 +0700

    Update [...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -18,5 +18,16 @@ export default NextAuth({
 	pages: {
 		signIn: '/auth/signin',
 	},
+	
 	adapter: MongoDBAdapter(clientPromise),
+	callbacks: {
+		async jwt({ token, account }) {
+		  // Persist the OAuth access_token to the token right after signin
+		  if (account) {
+			token.accessToken = account.access_token
+		  }
+		  return token
+		}
+	  }
 })
+
