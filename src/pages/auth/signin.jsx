08efa114632ad0b2fac575a2527a89cca415cdd0
ohaki <oom20170@hotmail.com>
2022-07-28 02:42:48 +0700

    delete button email

--- src/pages/auth/signin.jsx
+++ src/pages/auth/signin.jsx
@@ -28,7 +28,7 @@ const providers = [
 const Signin = () => {
 	const { data: session, status } = useSession()
 	const { push } = useRouter()
-	const [email, setEmail] = useState('')
+	
 
 	console.log(session)
 	if (status === 'loading') return <Heading>Checking Authentication...</Heading>
@@ -43,28 +43,11 @@ const Signin = () => {
 
 	const handleOAuthSignIn = (provider) => () => signIn(provider)
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
 
-		if (!email) return false
-
-		signIn('email', { email, redirect: false })
-	}
 
 	return (
 		<Box>
-			<chakra.form onSubmit={handleSubmit}>
-				<FormLabel>Email Address</FormLabel>
-				<Input
-					value={email}
-					type='email'
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-
-				<Button type='submit' w='100%' my={5}>
-					Login
-				</Button>
-			</chakra.form>
+		
 
 			<VStack>
 				{providers.map(({ name, Icon }) => (
