c59876834905a830f78308b921b6c0d6bd7742ed
ohaki <oom20170@hotmail.com>
2023-01-11 01:53:14 +0700

    update api

---
+++ pages/api/users/{studentId}.js
@@ -0,0 +1,23 @@
+import db from '../../../database/connectdata';
+import {getUser,putUser,deleteUser} from  '../../../database/usercontroller';
+
+export default async function indexuser(req,res){
+    db().catch(() => res.status(405).json({error:"error connect"}));
+  const{method} = req
+  switch(method){
+        case "GET":
+        getUser(req,res);
+        break;
+        case'PUT':
+        putUser(req,res);
+        break;
+        case'DELETE':
+        deleteUser(req,res);
+        break;
+        default:
+            res.setHeader('Allow',['GET','PUT','DELETE']);
+            res.status(405).end(`Method ${method} Not Allowd`);
+            break;
+  }
+    
+}
